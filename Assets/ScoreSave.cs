72b27c9e18c03f467b9d47b6258e8200e93b108f
senshirou <senshirou@hotmail.com>
2019-10-25 15:16:58 +0900

    UI Implementation

---
+++ Assets/ScoreSave.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSave : MonoBehaviour
+{
+
+    public GameObject score_object = null;
+    public int score_num = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //スコアのロード
+        score_num = PlayerPrefs.GetInt("SCORE", 0);
+
+
+    }
+
+    //削除時の処理
+    void OnDestroy()
+    {
+        //スコアを保存
+        PlayerPrefs.SetInt("SCORE", score_num);
+        PlayerPrefs.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text>();
+        //テキストの表示を入れ替える
+        score_text.text = "Score:" + score_num;
+
+
+        score_num += 1; //１を加算し続ける。
+        
+    }
+}
