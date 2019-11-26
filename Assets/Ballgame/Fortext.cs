5aab28c175be3e3d0407f8bcbb717b694574129c
senshirou <senshirou@hotmail.com>
2019-11-26 14:44:53 +0900

    add vest10 scoreresult

---
+++ Assets/Ballgame/Fortext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fortext : MonoBehaviour
+{
+    List<int> TestList;
+    int sono1, sono2, sono3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestList = new List<int> { sono1, sono2, sono3 };
+
+        for (int i = 0; i < 3; i++)
+        {
+            TestList[i] = i;
+            Debug.Log(i);
+        }
+
+        
+
+        Debug.Log(string.Join(",",TestList));
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Ballgame/ScoreControl2.cs
+++ Assets/Ballgame/ScoreControl2.cs
@@ -6,22 +6,40 @@ using System.Linq;
 
 public class ScoreControl2 : MonoBehaviour
 {
-
-    public List<int> ScoreResult;
+    public int Myscore;
+    int score0, score1, score2, score3, score4, score5, score6, score7, score8,score9;
+    List<int> ScoreResult;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i < 11; i++)
+        ScoreResult = new List<int> { score0, score1, score2, score3, score4, score5, score6, score7, score8, score9 };
+        for (int i = 0; i < ScoreResult.Count; i++)
         {
             ScoreResult[i] = PlayerPrefs.GetInt($"Score{i}");
-
         }
-        
+        Debug.Log(string.Join(",",ScoreResult));
     }
 
+    void OnDisable()
+    {
+        ScoreResult.Add(Myscore);
+        var ScoreResults = ScoreResult.OrderByDescending(s => s).ToArray();
+        for (int i = 0; i < ScoreResult.Count; i++)
+        {
+            PlayerPrefs.SetInt($"Score{i}", ScoreResults[i]);
+        }
+        ScoreResult.Remove(10);
+        Debug.Log(string.Join(",", ScoreResults));
+
+        PlayerPrefs.Save();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ++Myscore;
+            Debug.Log(Myscore);
+        }
     }
 }
